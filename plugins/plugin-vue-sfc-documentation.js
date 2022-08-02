b451f54ed1e43990188647ad614caf709bbe762e
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2022-08-02 12:09:29 -0400

    break things

--- plugins/plugin-vue-sfc-documentation.js
+++ plugins/plugin-vue-sfc-documentation.js
@@ -6,13 +6,6 @@ const blockRegex = new RegExp(blockRegString, 'g')
 const removeDocTag = (src)=>{
     return src.split(blockRegex)[0]
 }
-
-export default function myPlugin() {
-  return {
-    name: 'transform-file',
-    transform(src, id) {
-      if (fileRegex.test(id)) {
-        // copy doc to appropriate docs location
         return {
           code: removeDocTag(src),
           map: null // provide source map if available
