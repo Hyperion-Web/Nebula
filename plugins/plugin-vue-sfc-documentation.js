9bb163f693490da7472f2301c9321f722b103147
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2022-08-02 21:43:03 -0400

    bad edits

--- plugins/plugin-vue-sfc-documentation.js
+++ plugins/plugin-vue-sfc-documentation.js
@@ -1,11 +1,7 @@
 const fileRegex = /\.(vue)$/
 const docTag = 'doc'
 const blockRegString = `(<${docTag}>)(\\D*?)(<\/${docTag}>)`
-const blockRegex = new RegExp(blockRegString, 'g')
-
-const removeDocTag = (src)=>{
-    return src.split(blockRegex)[0]
-}
+const blosdf
         return {
           code: resdf 
     }
